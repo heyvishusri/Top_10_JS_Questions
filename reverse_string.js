66a4540a0d3d45409723504ad7e86b44f854e73c
heyvishusri <vishwaskumar55347@gmail.com>
2025-09-02 00:19:08 +0530

    updated code

---
+++ reverse_string.js
@@ -0,0 +1,10 @@
+// Write a function that returns the reverse of a given string.
+
+function reverseStr(Str) {
+  let reversedStr = "";
+  for (let i = Str.length - 1; i >= 0; i--) {
+    reversedStr += Str[i];
+  }
+  return reversedStr;
+}
+console.log(reverseStr("Hello RKU"));
